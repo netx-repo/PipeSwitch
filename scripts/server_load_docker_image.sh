d2660456c3c06a38e7fa0c131c560b8c73b52e24
Zhihao Bai <zbai1@jhu.edu>
2021-06-16 20:29:40 -0400

    Test

--- scripts/server_load_docker_image.sh
+++ scripts/server_load_docker_image.sh
@@ -1,20 +1,18 @@
+echo 'PipeSwitch demo: load Docker images'
+echo
 
-echo $(pwd)
+# Get current work dir
+WORK_DIR=$(pwd)/PipeSwitch
+echo 'Current work dir:' $WORK_DIR
+echo
 
-# echo 'PipeSwitch demo: load Docker images'
-# echo
+# Import global variables
+source $WORK_DIR/config/env.sh
+echo 'Import global variables'
+echo
 
-# # Get current work dir
-# WORK_DIR=$(pwd)
-# echo 'Current work dir:' $WORK_DIR
-# echo
-
-# # Import global variables
-# source $WORK_DIR/config/env.sh
-# echo 'Import global variables'
-# echo
-
-# # Import docker image
+# Import docker image
+echo $WORK_DIR
 # echo 'Import docker image:' $DOCKER_IMAGE_BASE_TAG
 # docker load -i ~/$DOCKER_IMAGE_BASE_FILENAME
 # echo 'Complete importing docker image:' $DOCKER_IMAGE_BASE_FILENAME
