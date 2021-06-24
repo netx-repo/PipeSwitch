90cb45a6d32acf55df2fa97763769f9d49aa6885
Zhihao Bai <zbai1@jhu.edu>
2021-06-23 20:42:16 -0400

    Rename dockerfile

--- scripts/host_build_docker_image.sh
+++ scripts/host_build_docker_image.sh
@@ -14,7 +14,7 @@ echo
 
 # Build the image bash, which compiles PyTorch from source
 echo 'Create docker image:' $DOCKER_IMAGE_BASE_TAG
-docker build -t $DOCKER_IMAGE_BASE_TAG $WORK_DIR/Dockerfile
+docker build -t $DOCKER_IMAGE_BASE_TAG $WORK_DIR/Dockerfile-base
 echo 'Complete creating docker image:' $DOCKER_IMAGE_BASE_TAG
 echo
 
