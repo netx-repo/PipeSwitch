1f7e7a92fda35e11b5335622c3683041afaf8b6d
Zhihao Bai <zbai1@jhu.edu>
2021-07-04 17:58:15 -0400

    Add script for figure5-ready_model-inception_v3

---
+++ scripts/figures/figure5/figure5_ready_model_inception_v3/remote_run_data.sh
@@ -0,0 +1,7 @@
+# Get current work dir
+WORK_DIR=$(pwd)/PipeSwitch
+
+# Import global variables
+source $WORK_DIR/scripts/config/env.sh
+
+PYTHONPATH=$PYTHONPATH:$WORK_DIR python $WORK_DIR/scripts/figures/figure5/figure5_ready_model_inception_v3/remote_run_data.py
