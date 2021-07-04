0b2708134e31d24fd3fc9e24239015aa44861796
Zhihao Bai <zbai1@jhu.edu>
2021-07-04 18:18:20 -0400

    Update for kill_restart

--- scripts/figures/figure5/figure5_ready_model_resnet152/remote_run_data.py
+++ scripts/figures/figure5/figure5_ready_model_resnet152/remote_run_data.py
@@ -3,7 +3,7 @@ import os
 from scripts.common.util import RunDocker
 
 def main():
-    with RunDocker('pipeswitch:pipeswitch', 'pipeswitch', 'figure5_ready_model_resnet152') as rd:
+    with RunDocker('pipeswitch:ready_model', 'pipeswitch', 'figure5_ready_model_resnet152') as rd:
         # Start the server: ready_model
         rd.run('python PipeSwitch/scripts/run_data.py')
         
