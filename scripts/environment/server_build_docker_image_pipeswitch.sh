ee46c666e6fa3059833390ff84086518c2b3e296
Zhihao Bai <zbai1@jhu.edu>
2021-07-03 23:48:25 -0400

    Debug

--- scripts/environment/server_build_docker_image_pipeswitch.sh
+++ scripts/environment/server_build_docker_image_pipeswitch.sh
@@ -1,15 +1,15 @@
-echo 'PipeSwitch demo: server creates Docker image for pipeswitch'
-echo
+# echo 'PipeSwitch demo: server creates Docker image for pipeswitch'
+# echo
 
 # Get current work dir
 WORK_DIR=$(pwd)/PipeSwitch
-echo 'Current work dir:' $WORK_DIR
-echo
+# echo 'Current work dir:' $WORK_DIR
+# echo
 
 # Import global variables
 source $WORK_DIR/scripts/config/env.sh
-echo 'Import global variables'
-echo
+# echo 'Import global variables'
+# echo
 
 # Build the image bash, which compiles PyTorch from source
 echo 'Create docker image:' $DOCKER_IMAGE_PIPESWITCH_TAG
--- scripts/figures/figure5/figure5_pipeswitch_resnet152/remote_run_data.sh
+++ scripts/figures/figure5/figure5_pipeswitch_resnet152/remote_run_data.sh
@@ -1,1 +1,14 @@
-python scripts/figures/figure5/figure5_pipeswitch_resnet152/remote_run_data.py
+# echo 'PipeSwitch demo: server creates Docker image for pipeswitch'
+# echo
+
+# Get current work dir
+WORK_DIR=$(pwd)/PipeSwitch
+# echo 'Current work dir:' $WORK_DIR
+# echo
+
+# Import global variables
+source $WORK_DIR/scripts/config/env.sh
+# echo 'Import global variables'
+# echo
+
+python $WORK_DIR/scripts/figures/figure5/figure5_pipeswitch_resnet152/remote_run_data.py
