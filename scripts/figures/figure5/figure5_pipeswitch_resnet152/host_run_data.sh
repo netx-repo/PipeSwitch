be324b556a68afec866eb90d88cda70851991c19
Zhihao Bai <zbai1@jhu.edu>
2021-07-04 02:09:49 -0400

    Polish output

--- scripts/figures/figure5/figure5_pipeswitch_resnet152/host_run_data.sh
+++ scripts/figures/figure5/figure5_pipeswitch_resnet152/host_run_data.sh
@@ -4,4 +4,5 @@ WORK_DIR=$(pwd)
 # Import global variables
 source $WORK_DIR/scripts/config/env.sh
 
+echo "Plot figure 5: ResNet152, PipeSwitch"
 PYTHONPATH=$PYTHONPATH:$WORK_DIR python scripts/figures/figure5/figure5_pipeswitch_resnet152/host_run_data.py $WORK_DIR/scripts/config/servers.txt
--- scripts/figures/figure5/plot_figure5.sh
+++ scripts/figures/figure5/plot_figure5.sh
@@ -4,4 +4,7 @@ WORK_DIR=$(pwd)
 # Import global variables
 source $WORK_DIR/scripts/config/env.sh
 
+echo "Plot figure 5"
 PYTHONPATH=$PYTHONPATH:$WORK_DIR python scripts/figures/figure5/host_run_figure.py
+echo "Complete ploting figure 5"
+echo
