f87feaf7978520ecad1e2ef1a4b569307995619b
Zhihao Bai <zbai1@jhu.edu>
2021-07-04 02:30:40 -0400

    Update for figure5-pipeswitch-inception_v3

---
+++ scripts/figures/figure5/figure5_pipeswitch_inception_v3/host_run_data.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+from scripts.common.util import RunRemoteRepo, import_server_list
+
+def main():
+    server_list_path = sys.argv[1]
+    print (server_list_path)
+
+    server_list = import_server_list(server_list_path)
+
+    with RunRemoteRepo(server_list[0], 'dev') as rrr:
+        rrr.run("bash ~/PipeSwitch/scripts/figures/figure5/figure5_pipeswitch_inception_v3/remote_run_data.sh")
+
+if __name__ == '__main__':
+    main()
