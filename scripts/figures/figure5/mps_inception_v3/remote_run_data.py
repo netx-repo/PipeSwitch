757ce81362a55450607eb754d7ba75e6dee9fbf8
Zhihao Bai <zbai1@jhu.edu>
2021-07-08 05:12:21 -0400

    Debug

--- scripts/figures/figure5/mps_inception_v3/remote_run_data.py
+++ scripts/figures/figure5/mps_inception_v3/remote_run_data.py
@@ -4,7 +4,7 @@ import sys
 from scripts.common.util import RunDocker
 
 def main():
-    with RunDocker('pipeswitch:ready_model', 'figure5_mps_inception_v3') as rd:
+    with RunDocker('pipeswitch:mps', 'figure5_mps_inception_v3') as rd:
         # Start the server: ready_model
         rd.run('python PipeSwitch/scripts/run_data.py')
         
